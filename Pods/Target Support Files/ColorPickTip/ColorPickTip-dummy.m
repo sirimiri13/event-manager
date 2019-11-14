948a4601e9c52a0c75debec7806cb818d088b463
HUONG <lamquynhhuong1304@gmail.com>
2019-11-14 18:06:49 +0700

    final commit

--- Pods/Target Support Files/ColorPickTip/ColorPickTip-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ColorPickTip : NSObject
-@end
-@implementation PodsDummy_ColorPickTip
-@end
--- Pods/Target Support Files/Pods-midterm/Pods-midterm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_midterm : NSObject
-@end
-@implementation PodsDummy_Pods_midterm
-@end
